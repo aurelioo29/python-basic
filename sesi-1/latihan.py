b85fa163b38e7f7ac463c7f5e6104ace44ca6680
Haloio29 <aureliolucio5@gmail.com>
2024-03-20 18:20:09 +0700

    add validation checker

--- sesi-1/latihan.py
+++ sesi-1/latihan.py
@@ -3,13 +3,26 @@ import random
 random = random.randint(1, 10)
 welcome = "Selamat datang di game tebak angka"
 
+print("=======================================")
 print(welcome)
+print("=======================================")
 input_user = input("Masukkan nama anda: ")
-print(f"Selamat bermain {input_user} \n")
+print("=======================================")
+print(f"Selamat bermain {input_user}")
+print("=======================================")
 tebakan_angka = int(input("Tebak angka dari 1-10: ")) # sudah di convery ke integer, karena bawaan input tipe data string
-print("\n")
-if tebakan_angka == random:
-    print("Selamat, tebakan anda benar")
-else:
-    print(f"Maaf, tebakan anda salah. Angka yang benar adalah {random}")
+print("=======================================")
+confirm_message = input(f"Apakah angka {tebakan_angka} sudah benar? (y/n): ")
+print("=======================================")
+if confirm_message == "y":
+    if tebakan_angka == random:
+        print("Selamat, tebakan anda benar")
+    else:
+        print(f"Maaf, tebakan anda salah. Angka yang benar adalah {random}")
+elif confirm_message == "n":
+    print("Anda membatalkan permainan")
+    exit()
+else :
+    print("Inputan tidak valid, program berhenti")
+    exit()
 
