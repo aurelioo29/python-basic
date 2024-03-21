d8b6b6f4addb7f3cbe8b5b878a2391ceaab924e6
Haloio29 <aureliolucio5@gmail.com>
2024-03-21 16:01:56 +0700

    Fix logic random bug

--- sesi-3/latihan.py
+++ sesi-3/latihan.py
@@ -3,46 +3,43 @@ import sys
 sys.path.insert(0, 'D:/Coding/website/python_basic/package')  # Using forward slashes
 import library
 
-random = random.randint(1, 10)
+goa = ["|_|"] * 10  # Goa Asli
 
 library.message_come("Permainan Tebak Angka Goa")
 
-gambar = "|_|"
-goa = [gambar] * 10 # ! Goa Asli
-
-temporary_goa = goa.copy() # ! Goa yang sudah diubah
-temporary_goa[random - 1] = "|X|"
-
-join_goa = " ".join(temporary_goa)
-
 input_user = input("Masukkan nama anda: ")
-
 while input_user == "":
     input_user = input("(Nama tidak boleh kosong) Masukkan nama anda: ")
-print("=======================================")
+
 print(f"Selamat bermain {input_user}")
-print("=======================================")
+
 while True:
-    tebakan_angka = int(input(f"{goa} \n Tebak angka Goa dari 1-10: ")) # sudah di convery ke integer, karena bawaan input tipe data string
-    while tebakan_angka < 1 or tebakan_angka > 10 :
+    random_number = random.randint(1, 10)  # Menghasilkan nomor acak baru setiap kali permainan dimulai
+    temporary_goa = goa.copy()  # Goa yang sudah diubah
+    temporary_goa[random_number - 1] = "|X|"
+    join_goa = " ".join(temporary_goa)
+
+    tebakan_angka = int(input(f"{goa} \n Tebak angka Goa dari 1-10: "))  # Sudah di convert ke integer, karena bawaan input tipe data string
+    while tebakan_angka < 1 or tebakan_angka > 10:
         input_user = input("(Angka tidak terdeteksi) Masukkan angka Goa dari 1-10: ")
         tebakan_angka = int(input_user)
-    print("=======================================")
     confirm_message = input(f"Apakah angka {tebakan_angka} sudah benar? (y/n): ")
-    print("=======================================")
     if confirm_message == "y":
-        if tebakan_angka == random:
+        if tebakan_angka == random_number:
             print(f"Selamat, tebakan anda benar. \n {join_goa}")
         else:
             print(f"Maaf, tebakan anda salah. \n {join_goa}")
     elif confirm_message == "n":
         print("Anda membatalkan permainan")
         exit()
-    else :
+    else:
         print("Inputan tidak valid, program berhenti")
         exit()
+    # *Validate user want to play again
     confirm_message = input("Apakah anda ingin bermain lagi? (y/n): ")
-    if confirm_message == "n":
+    if confirm_message == "n": # Jika inputan "n"
         break
-    
+    else: # Jika inputan selain "n / y"
+        exit()
+
 print("Terima kasih sudah bermain")
