3debb71e0f9d1113059670c281411f0fa1ee6f10
Haloio29 <aureliolucio5@gmail.com>
2024-03-21 09:32:38 +0700

    Add function message_come on folder package/library.py

---
+++ package/library.py
@@ -0,0 +1,7 @@
+def message_come():
+  print("=======================================")
+  print(f"Selamat datang di game tebak angka")
+  print("=======================================")
+  
+def tester():
+  return print("ini tester")
